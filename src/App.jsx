0c4ed04b8555012722d5c678882a85ba82a8d8b2
shabbar khan <shabbarkhan.js@gmail.com>
2024-11-06 19:08:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Components/Card";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1 className=" bg-black text-2xl text-white font-bold text-center">
+          tailwind css
+        </h1>
+      </div>
+
+      <Card />
+    
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="flex justify-center items-center flex-col lg:flex-row">
+      <div className=" h-[40vh] w-[100vw] lg:w-[33vw] bg-zinc-500 border-r-2 text-white">
+        12345
+      </div>
+      <div className=" h-[40vh] w-[100vw] lg:w-[33vw] bg-zinc-500 border-r-2 text-white">
+        12345
+      </div>
+      <div className=" h-[40vh] w-[100vw] lg:w-[33vw] bg-zinc-500 border-r-2 text-white">
+        12345
+      </div>
+    </div>
+  );
+};
+
+export default Card;
